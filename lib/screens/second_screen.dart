4a9a1f34fc427c459724916beb0afc01d3b297ee
rlaalsvkdl <police0418@gmail.com>
2024-04-17 21:30:00 +0900

    commit

---
+++ lib/screens/second_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/vnqkdh.jpg',
+                width: 300,
+                height: 300,
+              ),
+              Text(
+                'Panda',
+                style: TextStyle(
+                    fontFamily: 'Regular',
+                    fontSize: 50
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
